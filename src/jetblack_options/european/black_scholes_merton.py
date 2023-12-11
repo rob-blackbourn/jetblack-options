5a2bb73c3f955e2a412548b3610a78bcc14c9975
Rob Blackbourn <rob.blackbourn@gmail.com>
2023-12-11 17:57:32 +0000

    Added more docs

--- src/jetblack_options/european/black_scholes_merton.py
+++ src/jetblack_options/european/black_scholes_merton.py
@@ -1,4 +1,4 @@
-"""Black-Scholes-Merton options pricing formulae using dividend yield.
+r"""Black-Scholes-Merton options pricing formulae using dividend yield.
 
 * Stock price $ S $,
 * Strike price $ K $,
@@ -6,6 +6,24 @@
 * Annual dividend yield $ q $,
 * Time to maturity $ \tau = T - t $
 * Volatility $ \sigma $.
+
+where:
+
+$$
+d_1 &= \frac{\ln(S/K) + \left(r - q + \frac{1}{2}\sigma^2\right)\tau}{\sigma\sqrt{\tau}}
+$$
+
+$$
+d_2 &= \frac{\ln(S/K) + \left(r - q - \frac{1}{2}\sigma^2\right)\tau}{\sigma\sqrt{\tau}} = d_1 - \sigma\sqrt{\tau}
+$$
+
+$$
+\varphi(x) &= \frac{1}{\sqrt{2\pi}} e^{-\frac{1}{2} x^2}
+$$
+
+$$
+\Phi(x) &= \frac{1}{\sqrt{2\pi}} \int_{-\infty}^x e^{-\frac{1}{2} y^2} \,dy = 1 - \frac{1}{\sqrt{2\pi}} \int_x^\infty e^{-\frac{1}{2} y^2} \,dy
+$$
 """
 
 from math import exp, log, pi, sqrt
@@ -31,7 +49,7 @@ def price(
         q: float,
         v: float,
 ) -> float:
-    """The fair value of a European option, using Black-Scholes-Merton.
+    r"""The fair value of a European option, using Black-Scholes-Merton.
 
     Call price: $Se^{-q \tau}\Phi(d_1) - e^{-r \tau} K\Phi(d_2)$
 
@@ -112,7 +130,7 @@ def delta(
         q: float,
         v: float,
 ) -> float:
-    """The sensitivity of the open to a change in the asset price.
+    r"""The sensitivity of the open to a change in the asset price.
 
     Call $\Delta$  $e^{-q \tau} \Phi(d_1)$
 
@@ -146,9 +164,11 @@ def gamma(
         q: float,
         v: float,
 ) -> float:
-    """The second derivative to the change in the asset price.
+    r"""The second derivative to the change in the asset price.
 
-    $ \Gamma $ $ e^{-q \tau} \frac{\varphi(d_1)}{S\sigma\sqrt{\tau}} = K e^{-r \tau} \frac{\varphi(d_2)}{S^2\sigma\sqrt{\tau}} $
+    $$
+    \Gamma $ $ e^{-q \tau} \frac{\varphi(d_1)}{S\sigma\sqrt{\tau}} = K e^{-r \tau} \frac{\varphi(d_2)}{S^2\sigma\sqrt{\tau}}
+    $$
 
     Args:
         S (float): The current asset price.
@@ -175,11 +195,15 @@ def theta(
         q: float,
         v: float,
 ) -> float:
-    """The theta or time decay of the value of the option.
+    r"""The theta or time decay of the value of the option.
 
-    $ Call \Theta $ $ - e^{-q \tau} \frac{S \varphi(d_1) \sigma}{2 \sqrt{\tau}} - rKe^{-r \tau}\Phi(d_2) + qSe^{-q \tau}\Phi(d_1) $
+    $$
+    Call \Theta $ $ - e^{-q \tau} \frac{S \varphi(d_1) \sigma}{2 \sqrt{\tau}} - rKe^{-r \tau}\Phi(d_2) + qSe^{-q \tau}\Phi(d_1)
+    $$
 
-    $ Put \Theta $ $ - e^{-q \tau}\frac{S \varphi(d_1) \sigma}{2 \sqrt{\tau}} + rKe^{-r \tau}\Phi(-d_2) - qSe^{-q \tau}\Phi(-d_1) $
+    $$
+    Put \Theta $ $ - e^{-q \tau}\frac{S \varphi(d_1) \sigma}{2 \sqrt{\tau}} + rKe^{-r \tau}\Phi(-d_2) - qSe^{-q \tau}\Phi(-d_1)
+    $$
 
     Args:
         is_call (bool): True for a call, false for a put.
@@ -216,9 +240,11 @@ def vega(
         q: float,
         v: float,
 ) -> float:
-    """The sensitivity of the options price or a change in the asset volatility.
+    r"""The sensitivity of the options price or a change in the asset volatility.
 
-    $ \mathcal{V} $ is $ S e^{-q \tau} \varphi(d_1) \sqrt{\tau} = K e^{-r \tau} \varphi(d_2) \sqrt{\tau} $
+    $$
+    \mathcal{V} $ is $ S e^{-q \tau} \varphi(d_1) \sqrt{\tau} = K e^{-r \tau} \varphi(d_2) \sqrt{\tau}
+    $$
 
     Args:
         S (float): The current asset price.
@@ -244,7 +270,7 @@ def rho(
         q: float,
         v: float,
 ) -> float:
-    """The sensitivity of the option price to the risk free rate.
+    r"""The sensitivity of the option price to the risk free rate.
 
     Call $ \rho $ is $ K \tau e^{-r \tau}\Phi(d_2) $
 
@@ -380,9 +406,11 @@ def vanna(
         q: float,
         v: float,
 ) -> float:
-    """The sensitivity to the spot price and volatility.
+    r"""The sensitivity to the spot price and volatility.
 
-    $ -e^{-q \tau} \varphi(d_1) \frac{d_2}{\sigma} \, = \frac{\mathcal{V}}{S}\left[1 - \frac{d_1}{\sigma\sqrt{\tau}} \right] $
+    $$
+    -e^{-q \tau} \varphi(d_1) \frac{d_2}{\sigma} \, = \frac{\mathcal{V}}{S}\left[1 - \frac{d_1}{\sigma\sqrt{\tau}} \right]
+    $$
 
     Also known as DdeltaDvol.
 
